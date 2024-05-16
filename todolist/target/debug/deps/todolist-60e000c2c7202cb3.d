8fc8c5156f0d7215de2bb93783ec3e4b0b50def5
Mishal Turkane <mishalturkane@gmail.com>
2024-05-16 12:43:12 +0530

    tempreture_converter added

---
+++ todolist/target/debug/deps/todolist-60e000c2c7202cb3.d
@@ -0,0 +1,5 @@
+d:\rustprojects\rust-projects\todolist\target\debug\deps\libtodolist-60e000c2c7202cb3.rmeta: src\main.rs
+
+d:\rustprojects\rust-projects\todolist\target\debug\deps\todolist-60e000c2c7202cb3.d: src\main.rs
+
+src\main.rs:
---
+++ todolist/target/debug/deps/todolist-e306970715cd32c9.d
@@ -0,0 +1,5 @@
+d:\rustprojects\rust-projects\todolist\target\debug\deps\libtodolist-e306970715cd32c9.rmeta: src\main.rs
+
+d:\rustprojects\rust-projects\todolist\target\debug\deps\todolist-e306970715cd32c9.d: src\main.rs
+
+src\main.rs:
